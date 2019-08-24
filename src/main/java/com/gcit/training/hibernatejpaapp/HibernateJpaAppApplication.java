310cfd97dbae9e8e335362c744cbf2cf8b639211
Allen-C-Chen <52220585+Allen-C-Chen@users.noreply.github.com>
2019-08-24 02:27:21 -0400

    Update HibernateJpaAppApplication.java

--- src/main/java/com/gcit/training/hibernatejpaapp/HibernateJpaAppApplication.java
+++ src/main/java/com/gcit/training/hibernatejpaapp/HibernateJpaAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HibernateJpaAppApplication {
 	//Allen is here testing webhooks
 	//testing two
+	//testing three
 	public static void main(String[] args) {
 		SpringApplication.run(HibernateJpaAppApplication.class, args);
 		
